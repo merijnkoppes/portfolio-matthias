7e15b40a97a64b5e5cfb2d2146f128346818f099
MerijnKoppes <koppesmerijn@gmail.com>
2024-10-02 19:33:22 +0200

    end of day

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { VideoComponent } from './components/video/video.component';
+import { PhotoComponent } from './components/photo/photo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'video', component: VideoComponent },
+  { path: 'photo', component: PhotoComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
